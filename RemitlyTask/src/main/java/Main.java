d2c467706752904553de3b406b1480902c5da835
MMRasbb <maciek.malinowski.poczta@gmail.com>
2024-04-23 15:25:04 +0200

    fixed gitignore

--- RemitlyTask/src/main/java/Main.java
+++ RemitlyTask/src/main/java/Main.java
@@ -1,4 +1,10 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collector;
+
 public class Main {
+
     public static void main(String[] args){
         JsonVerifier jsonVerifier = new JsonVerifier();
         System.out.println(jsonVerifier.isLegit(args[0]));
